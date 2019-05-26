2e10712736f58df3fad06ee18dd12e38b8e389cd
emilie <emilie.dannenberg@gmail.com>
2019-05-26 16:30:02 -0500

    initialize firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
 
 import { HomePage } from '../pages/home/home';
 @Component({
@@ -16,6 +18,22 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+      this.initialiseFirebase(); 
+
+
+    });
+  }
+
+  initialiseFirebase() {
+
+    firebase.initializeApp({
+      apiKey: "AIzaSyDzH3b0vtk00IMJkAHzrAb_Y81-4-YJiWg",
+      authDomain: "nchat-7bd57.firebaseapp.com",
+      databaseURL: "https://nchat-7bd57.firebaseio.com",
+      projectId: "nchat-7bd57",
+      storageBucket: "nchat-7bd57.appspot.com",
+      messagingSenderId: "1023127763832",
+      appId: "1:1023127763832:web:eb84d4c4d1746627"
     });
   }
 }
